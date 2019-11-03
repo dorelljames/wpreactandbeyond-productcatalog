61b3f5c014902294f2fce858917714899974a976
Dorell James <galangdj@gmail.com>
2019-11-03 14:50:21 +0800

    Add search functionality by filtering products by name

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { useQuery } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
@@ -25,28 +25,61 @@ const GET_PRODUCTS = gql`
 `;
 
 function App() {
-  const { loading, error, data } = useQuery(GET_PRODUCTS);
-  console.log(data);
+  const [products, setProducts] = useState([]);
+  const { error, loading, data } = useQuery(GET_PRODUCTS);
+
+  useEffect(() => {
+    if (error) {
+      setProducts([]);
+    }
+
+    if (!loading && data && data.products) {
+      setProducts(data.products.edges.map(p => p.node));
+    }
+  }, [error, loading, data]);
+
+  const searchProducts = e => {
+    const currentSearch = e.target;
+
+    setProducts(
+      currentSearch.value === ""
+        ? data.products.edges.map(p => p.node)
+        : prevProducts =>
+            prevProducts.filter(product => {
+              return product.name
+                .toLowerCase()
+                .includes(currentSearch.value.toLowerCase());
+            })
+    );
+  };
 
   return (
     <div className="productCatalog">
       <h2>WP React and Beyond Catalog</h2>
       <div className="productSearch">
-        <input type="text" placeholder="Search for a product" />
+        <input
+          type="text"
+          placeholder="Search for a product"
+          onChange={e => searchProducts(e)}
+        />
       </div>
       <div className="productList">
-        {(loading && <p className="loading">Fetching products...</p>) ||
+        {(loading && <p className="text-white">Fetching products...</p>) ||
           (error && <p>Error :(</p>) ||
-          data.products.edges.map(product => (
-            <Product
-              key={product.node.id}
-              imgSrc={product.node.image.sourceUrl}
-              name={product.node.name}
-              description={product.node.description}
-              link={"products/" + product.node.slug}
-              price={product.node.price}
-              extraClass="blue"
-            />
+          (products.length >= 1 ? (
+            products.map(product => (
+              <Product
+                key={product.id}
+                imgSrc={product.image.sourceUrl}
+                name={product.name}
+                description={product.description}
+                link={"products/" + product.slug}
+                price={product.price}
+                extraClass="blue"
+              />
+            ))
+          ) : (
+            <p className="text-white">No products...</p>
           ))}
       </div>
     </div>
